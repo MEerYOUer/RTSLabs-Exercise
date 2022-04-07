703d1ee54bc67e785b921ad8badfeb8b009ca675
MEerYOUer <somethingeasytoremember@live.com>
2022-04-07 14:02:27 -0400

    add files

---
+++ RTSLabs-Exercise/Exercise.cs
@@ -0,0 +1,23 @@
+﻿namespace RTSLabs_Exercise
+{
+    public static class Exercise
+    {
+        public static Dictionary<string, int> AboveBelow(int[] unsorted, int comparison)
+        {
+            return new Dictionary<string, int>()
+            {
+                { "above", unsorted.Where(x => x > comparison).Count() },
+                { "below", unsorted.Where(x => x < comparison).Count() },
+            };
+        }
+
+        public static string StringRotation(string original, uint rotation)
+        {
+            if (rotation > original.Length)
+                throw new ArgumentException("The provided rotation is greater than the length of the string.");
+
+            var casted = (int)rotation; // substring doesn't accept unit
+            return original.Substring(original.Length - casted, casted) + original.Substring(0, original.Length - casted);
+        }
+    }
+}
---
+++ RTSLabs-Exercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using RTSLabs_Exercise;
+
+try
+{
+    // int comparison
+    var comparison = 6;
+    var unsorted = new int[] { 1, 5, 2, 1, 10, 6, 39024324, -3422 };
+    var abResult = Exercise.AboveBelow(unsorted, comparison);
+    var resultString = string.Join(", ", abResult.Select(x => x.Key + ": " + x.Value).ToArray());
+
+    Console.WriteLine($"Unsorted: [{string.Join(", ", unsorted)}]\nComparison: {comparison}");
+    Console.WriteLine($"AB Result: {resultString}");
+    Console.WriteLine();
+
+    // string rotation
+    uint rotation = 2;
+    var original = "MyString";
+    var shiftedResult = Exercise.StringRotation(original, rotation);
+
+    Console.WriteLine($"Original string: {original}");
+    Console.WriteLine($"Shifted result: {shiftedResult}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception: Message: [{ex.Message}]\nStack trace: {ex.StackTrace}");
+}
